7ca19fa98cc5f2df2fbc27cf0ddbe9d32d5ff1fc
helloworldjay <sj.seungjin.baek@gmail.com>
2021-12-30 19:50:45 +0900

    DEV-19 Response를 프로젝트에 필요한 모델로 변경하는 기능 구현

--- DaumCafeSearch/DaumCafeSearch/Repository/CafeSearchRepository.swift
+++ DaumCafeSearch/DaumCafeSearch/Repository/CafeSearchRepository.swift
@@ -14,6 +14,18 @@ protocol CafeSearchRepositoryLogic {
 // MARK: - Repository
 
 final class CafeSearchRepository: CafeSearchRepositoryLogic {
+  private func parseCafeList(of response: DaumCafeSearchResponse) -> [CafeArticle] {
+    return response.cafeArticles.map {
+      CafeArticle(
+        cafeName: $0.cafeName,
+        content: String(htmlEncodedString: $0.content) ?? $0.content,
+        datetime: $0.dateTime,
+        thumbnailURL: URL(string: $0.thumbnail),
+        title: String(htmlEncodedString: $0.title) ?? $0.title,
+        url: URL(string: $0.url)
+      )
+    }
+  }
 
 
 // MARK: - Edit HTML String
