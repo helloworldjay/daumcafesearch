7f812c919d0d9a89cfcd4508e8680c734e898c79
helloworldjay <sj.seungjin.baek@gmail.com>
2021-12-30 21:26:12 +0900

    DEV-21 SwiftUI Preview 기능 구현

--- DaumCafeSearch/DaumCafeSearch/ViewControllers/List/CafeSearchListViewController.swift
+++ DaumCafeSearch/DaumCafeSearch/ViewControllers/List/CafeSearchListViewController.swift
@@ -6,9 +6,26 @@
 //
 
 import UIKit
+import SwiftUI
 
 final class CafeSearchListViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
+
+
+// MARK: - Preview
+
+struct MainViewController_Previews: PreviewProvider {
+  static var previews: some View {
+    Container().edgesIgnoringSafeArea(.all)
+  }
+  
+  struct Container: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+      let rootViewController = CafeSearchListViewController()
+      return UINavigationController(rootViewController: rootViewController)
+    }
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+    typealias UIViewControllerType = UIViewController
   }
 }
