9a0cdc78fab7f1ab037f73210eb28cbda74c9058
helloworldjay <sj.seungjin.baek@gmail.com>
2021-12-30 20:58:44 +0900

    DEV-20 ViewModel 및 ViewModelLogic 프로토콜 생성

---
+++ DaumCafeSearch/DaumCafeSearch/ViewModels/CafeSearchViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  CafeSearchViewModel.swift
+//  DaumCafeSearch
+//
+//  Created by Seungjin Baek on 2021/12/30.
+//
+
+import Foundation
+
+protocol CafeSearchViewModelLogic {
+}
+
+final class CafeSearchViewModel: CafeSearchViewModelLogic {
+}
