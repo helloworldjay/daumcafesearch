eb56406b9db9726349b372f8f64161a8cedf91db
helloworldjay <sj.seungjin.baek@gmail.com>
2021-12-30 21:00:11 +0900

    DEV-20 ViewModel에 필요한 프로퍼티 생성

--- DaumCafeSearch/DaumCafeSearch/ViewModels/CafeSearchViewModel.swift
+++ DaumCafeSearch/DaumCafeSearch/ViewModels/CafeSearchViewModel.swift
@@ -8,7 +8,21 @@
 import Foundation
 
 protocol CafeSearchViewModelLogic {
+  var repository: CafeSearchRepositoryLogic? { get set }
+  var loadingStarted: () -> Void { get set }
+  var loadingEnded: () -> Void { get set }
+  var cafeArticleListUpdated: () -> Void { get set }
 }
 
 final class CafeSearchViewModel: CafeSearchViewModelLogic {
+  
+  // MARK: Properties
+  
+  var repository: CafeSearchRepositoryLogic?
+  var loadingStarted: () -> Void = { }
+  var loadingEnded: () -> Void = { }
+  var cafeArticleListUpdated: () -> Void = { }
+  
+  private var cafeArticleList: [CafeArticle] = []
+  private var isLoading = false
 }
