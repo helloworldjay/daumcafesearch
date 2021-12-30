896ac21177cc4b372c6270c6f884db4b07c73950
helloworldjay <sj.seungjin.baek@gmail.com>
2021-12-30 19:48:10 +0900

    DEV-19 문자열에서 HTML 태그를 제거하는 기능 추가

--- DaumCafeSearch/DaumCafeSearch/Repository/CafeSearchRepository.swift
+++ DaumCafeSearch/DaumCafeSearch/Repository/CafeSearchRepository.swift
@@ -14,4 +14,22 @@ protocol CafeSearchRepositoryLogic {
 // MARK: - Repository
 
 final class CafeSearchRepository: CafeSearchRepositoryLogic {
+
+
+// MARK: - Edit HTML String
+
+extension String {
+  init?(htmlEncodedString: String) {
+    guard let data = htmlEncodedString.data(using: .utf8) else {
+      return nil
+    }
+    let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+      .documentType: NSAttributedString.DocumentType.html,
+      .characterEncoding: String.Encoding.utf8.rawValue
+    ]
+    guard let attributedString = try? NSAttributedString(data: data, options: options, documentAttributes: nil) else {
+      return nil
+    }
+    self.init(attributedString.string)
+  }
 }
