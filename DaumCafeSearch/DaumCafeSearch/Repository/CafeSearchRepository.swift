7856466efcdda6e5837219ef46f8c4ded1818f96
helloworldjay <sj.seungjin.baek@gmail.com>
2021-12-30 19:46:34 +0900

    DEV-19 Repository 및 RepositoryLogic 프로토콜 생성

---
+++ DaumCafeSearch/DaumCafeSearch/Repository/CafeSearchRepository.swift
@@ -0,0 +1,17 @@
+//
+//  CafeSearchRepository.swift
+//  DaumCafeSearch
+//
+//  Created by Seungjin Baek on 2021/12/30.
+//
+
+import Foundation
+
+protocol CafeSearchRepositoryLogic {
+}
+
+
+// MARK: - Repository
+
+final class CafeSearchRepository: CafeSearchRepositoryLogic {
+}
